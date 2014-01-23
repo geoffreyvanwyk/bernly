275a53c29c097b56f3763bc30b45a74e2648b75f
Geoffrey van Wyk <geoffrey.vanwyk@bernadine.biz>
2014-01-23 14:33:37 +0200

    Added descriptive files. Edited DB connection settings.

--- configuration.php
+++ configuration.php
@@ -1,7 +1,7 @@
 <?php
 	define('DB_HOST_NAME', 'localhost');
 	define('DB_NAME', 'bernly');
-	define('DB_USERNAME', 'root');
-	define('DB_PASSWORD', 'T02qYxtGgFMo2OOtkjEp');
+	define('DB_USERNAME', 'bernly');
+	define('DB_PASSWORD', 's8uzmP8UR7fydYQZ');
 	define('APP_HOST_NAME', 'bernly.com');
 	define('APP_PATH', '/~geoffrey/bernly/');

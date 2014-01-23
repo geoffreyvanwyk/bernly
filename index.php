3446b676d7066a690e23f0db3bfa526f9436fee1
Geoffrey van Wyk <geoffrey.vanwyk@bernadine.biz>
2014-01-23 10:19:17 +0200

    First commit. index.php with form, but without functionality. bower_components.

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>URL Shorterner | bernly.com</title>
+		<link href="bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+	</head>
+	<body>
+		<div class="container">
+			<form action="." method="get">
+				<div class="form-group">
+					<input type="url" name="long" placeholder="Long URL" class="form-control">
+				</div>
+				<button type="submit" name="submit" class="btn btn-success form-control">Shorten</button>
+			</form>
+		</div>
+	</body>
+</html>

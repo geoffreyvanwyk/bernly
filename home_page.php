9b0a2573f4a738edd684a5352c9a815c4aec67e1
Geoffrey van Wyk <geoffrey.vanwyk@bernadine.biz>
2014-01-26 18:26:00 +0200

    Added a .jumbotron to home_page.php. Centered the logo and added a slogan. Increased the height of the text input box.

--- home_page.php
+++ home_page.php
@@ -6,30 +6,44 @@
 		<link href="bower_components/bootstrap/dist/css/bootstrap-theme.min.css" rel="stylesheet">
 		<style>
 			body {
-				padding-top: 50px; /* 40px to make the container go all the way to the bottom of the topbar */
+				padding-top: 0px; /* 40px to make the container go all the way to the bottom of the topbar */
 			}
 		</style>
 	</head>
 	<body>
-		<div class="container">
-			<img src="./bernly-logo.png" width="200" height="100">
-			<br><br><br><br>
-			<form action="." method="get">
-				<div class="input-group">
-					<span class="input-group-addon">
-						<span class="glyphicon glyphicon-link"></span>
-					</span>
-					<input type="url" name="long_url" placeholder="Paste long URL here" class="form-control">
-					<span class="input-group-btn">
-						<button type="submit" name="submit" class="btn btn-success">Shorten</button>
-					</span>
+		<div class="jumbotron">
+			<div class="container">
+				<div class="page-header">
+					<a href="/">
+						<img src="./bernly-logo.png" width="200" height="100" class="center-block">
+					</a>
+					<p class="lead text-center">Cut your links down to size.</p>
 				</div>
-			</form>
-
-		<?php if (isset($short_url)): ?>
-			<p><strong>Short URL </strong><?php echo $short_url; ?></p>
-		<?php endif; ?>
-
+				<form action="." method="get">
+					<div class="input-group" style="font-size: 14px !important">
+						<span class="input-group-addon">
+							<span class="glyphicon glyphicon-link"></span>
+						</span>
+						<input type="url" name="long_url" placeholder="Paste long link here"
+							class="form-control input-lg">
+						<span class="input-group-btn">
+							<button type="submit" name="submit" class="btn btn-success btn-lg">Shorten</button>
+						</span>
+					</div>
+				</form>
+			</div>
 		</div>
+
+		<div class="container">
+		</div>
+
+	<?php if (isset($short_url)): ?>
+		<br><br>
+		<div class="jumbotron">
+			<div class="container">
+				<h3><?php echo $short_url; ?></h3>
+			</div>
+		</div>
+	<?php endif; ?>
 	</body>
 </html>

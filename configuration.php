2ff739ba2cdd650276da69873faaac80abdeba74
Geoffrey van Wyk <geoffrey.vanwyk@bernadine.biz>
2014-04-11 15:06:41 +0200

    Reads configuration from configuration.json file.
    Configuration file is not part of version control.

--- configuration.php
+++ configuration.php
@@ -1,7 +1,11 @@
 <?php
-	define('DB_HOST_NAME', 'localhost');
-	define('DB_NAME', 'bernly');
-	define('DB_USERNAME', 'bernly');
-	define('DB_PASSWORD', 's8uzmP8UR7fydYQZ');
-	define('APP_HOST_NAME', 'localhost');
-	define('APP_PATH', '/~geoffrey/bernly/');
+	$config_json_string = file_get_contents("configuration.json");
+	$config_array = json_decode($config_json_string, true);
+	
+	define('DB_HOST_NAME', $config_array['database']['host']);
+	define('DB_NAME', $config_array['database']['name']);
+	define('DB_USERNAME', $config_array['database']['username']);
+	define('DB_PASSWORD', $config_array['database']['password']);
+	
+	define('APP_HOST_NAME', $config_array['app']['host']);
+	define('APP_PATH', $config_array['app']['path']);

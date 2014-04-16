624db9b97a3c70f65d81f2cce5aca73f93f60d87
Geoffrey van Wyk <geoffrey.vanwyk@bernadine.biz>
2014-04-16 19:29:58 +0200

    Laravel displays bernly home page in response to '/' request.
    Changed ownership recursively to geoffrey:www.

--- app/views/home-page.php
+++ app/views/home-page.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+
 <html>
 	<head>
 		<meta charset="utf-8">
@@ -7,28 +8,43 @@
 		<meta name="description" content="A link (URL, web address) shortener.">
 		<meta name="keywords" content="link, URL, web address, address, shorter, shorten, link shortener">
 
-		<link href="static/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+		<link href="<?= Config::get('app.url') ?>/../static/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 	
 		<title>Link Shorterner | bernly.com</title>
 	</head>
+	
 	<body>
 		<div class="jumbotron">
 			<div class="container">
 				<div class="page-header">
-					<a href="<?=APP_PATH?>">
-						<img src="static/img/bernly-logo.png" width="200" height="100" class="center-block">
+					<a href="<?= Config::get('app.url') ?>">
+						<img 
+							src="<?= Config::get('app.url') ?>/../static/img/bernly-logo.png" 
+							width="200" 
+							height="100" 
+							class="center-block"
+						>
 					</a>
 					<p class="lead text-center">Cut your links down to size.</p>
 				</div>
+				
 				<form action="." method="get">
 					<div class="input-group" style="font-size: 14px !important">
 						<span class="input-group-addon">
 							<span class="glyphicon glyphicon-link"></span>
 						</span>
-						<input type="url" name="long_url" placeholder="Paste long link here"
-							class="form-control input-lg">
+						
+						<input 
+							type="url" 
+							name="long_url" 
+							placeholder="Paste long link here"
+							class="form-control input-lg"
+						>
+						
 						<span class="input-group-btn">
-							<button type="submit" name="submit" class="btn btn-success btn-lg">Shorten</button>
+							<button type="submit" name="submit" class="btn btn-success btn-lg">
+								Shorten
+							</button>
 						</span>
 					</div>
 				</form>
@@ -39,11 +55,14 @@
 		</div>
 
 	<?php if (isset($short_url)): ?>
+	
 		<div class="container">
 			<div class="page-header">
 				<h3>Result<span class="pull-right"><strong><?=$short_url?></strong></span></h3>
 			</div>
 		</div>
+		
 	<?php endif; ?>
+	
 	</body>
 </html>

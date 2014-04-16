b0139efa664fe4b90732e5f4aaa408b931fa39c6
Geoffrey van Wyk <geoffrey.vanwyk@bernadine.biz>
2014-04-17 00:29:27 +0200

    Can now redirect a short URL to the corresponding long URL.

--- app/routes.php
+++ app/routes.php
@@ -11,21 +11,31 @@
 |
 */
 
+/* Display home page, and, if any, results of URL shortening. */
 Route::get('/', function()
 {
 	return View::make('home-page')->with('short_url', Session::get('short_url'));
 });
 
-/* Create a short url. */
+/* For a given long URL, create a short URL. */
 Route::post('/', function() 
 {
+	$long_url = Input::get('long_url');
 	$count = Url::all()->count();
 	$short_url = base_convert($count + 1, 10, 36);
 	
 	$url = new Url;
-	$url->long_url = Input::get('long_url');
+	$url->long_url = $long_url;
 	$url->short_url = $short_url;
 	$url->save();
 	
 	return Redirect::to('/')->with('short_url', $short_url);
 });
+
+/* For a given short URL, redirect to the corresponding long URL. */
+Route::get('{short_url}', function($short_url)
+{
+	$url = Url::where('short_url', '=', (int)$short_url)->first();
+	return Redirect::to($url['long_url']);
+})
+->where('short_url', '[^/]');

febee64acfbe0da366216c437c0dc748c1a28dd3
Geoffrey van Wyk <geoffrey.vanwyk@bernadine.biz>
2014-04-21 21:35:54 +0200

    Moved the URL shortening form from the layout to the home page.

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -1,7 +1,46 @@
 @extends('layouts.master')
 
 @section('content')
+
+	<div class="jumbotron" style="margin-bottom: 0px">
+		<div class="container">
+			<div class="page-header">
+				<a href="/">
+					<img 
+						src="static/img/bernly-logo.png" 
+						width="200" 
+						height="100" 
+						class="center-block"
+					>
+				</a>
+				<p class="lead text-center">Cut your links down to size.</p>
+			</div>
+			
+			<form action="." method="post">
+				<div class="input-group" style="font-size: 14px !important">
+					<span class="input-group-addon">
+						<span class="glyphicon glyphicon-link"></span>
+					</span>
+					
+					<input 
+						class="form-control input-lg"
+						type="url" 
+						name="long_url" 
+						placeholder="Paste long link here"
+					>
+					
+					<span class="input-group-btn">
+						<button class="btn btn-success btn-lg" type="submit" name="submit" >
+							Shorten
+						</button>
+					</span>
+				</div>
+			</form>
+		</div>
+	</div>
+	
 	@if ( isset( $short_url ) )
+	
 		<div class="container">
 			<div class="page-header">
 				<h3>Result
@@ -13,5 +52,6 @@
 				</h3>
 			</div>
 		</div>
+		
 	@endif
 @stop
--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -14,7 +14,6 @@
 	</head>
 	
 	<body>
-		<!-- Static navbar -->
 		<div class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0px">
 			<div class="container" >
 				<div class="navbar-header">
@@ -39,43 +38,6 @@
 			</div>
 		</div>
 
-		<div class="jumbotron" style="margin-bottom: 0px">
-			<div class="container">
-				<div class="page-header">
-					<a href="/">
-						<img 
-							src="static/img/bernly-logo.png" 
-							width="200" 
-							height="100" 
-							class="center-block"
-						>
-					</a>
-					<p class="lead text-center">Cut your links down to size.</p>
-				</div>
-				
-				<form action="." method="post">
-					<div class="input-group" style="font-size: 14px !important">
-						<span class="input-group-addon">
-							<span class="glyphicon glyphicon-link"></span>
-						</span>
-						
-						<input 
-							class="form-control input-lg"
-							type="url" 
-							name="long_url" 
-							placeholder="Paste long link here"
-						>
-						
-						<span class="input-group-btn">
-							<button class="btn btn-success btn-lg" type="submit" name="submit" >
-								Shorten
-							</button>
-						</span>
-					</div>
-				</form>
-			</div>
-		</div>
-		
 		@yield('content')
 		
 	    <div class="jumbotron">

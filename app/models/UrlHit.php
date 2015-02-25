a3ce9c5ba9765f61fadf4aa5d0b487b1b6f17dbf
Geoffrey van Wyk <geoffrey@vanwyk.biz>
2015-02-25 22:22:09 +0200

    Delete everything in preparation for Laravel 5.

--- app/models/UrlHit.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class UrlHit extends Eloquent 
-{
-    public function urls()
-    {
-        return $this->belongsTo( 'URL' );
-    }
-}
--- app/views/emails/verify.blade.php
+++
@@ -1,9 +0,0 @@
-Dear Sir/Madam,
-
-Thank you for registering an account on {{ Config::get( 'app.url' ) }}.
-
-Please click the following link to verify that you own this email address: 
-{{ Config::get( 'app.url' ) }}/verify/email?address={{ Auth::user()->email }}&token={{ $token }} .
-
-Kind regards,
-The Bernly Team

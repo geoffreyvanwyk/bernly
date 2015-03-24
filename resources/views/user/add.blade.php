5abdd4fea07ce95bb2d1d48ed50d76099bc4d48b
Geoffrey van Wyk <geoffrey@vanwyk.biz>
2015-03-24 23:07:00 +0200

    Adds cross-site scripting token to home page.

--- resources/views/user/add.blade.php
+++ resources/views/user/add.blade.php
@@ -7,6 +7,7 @@
         <h1 class="page-header">Register</h1>
     
         <form action="/user/add" method="post" role="form">
+            <input type="hidden" name="_token" value="{{ csrf_token() }}">
         
             <div class="form-group {{ Session::get( 'email_class' ) }}">
                 <label for="email" class="control-label">Email address</label>
